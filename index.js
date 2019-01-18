d1d6c4370e5f205a469e086e149a58b3bac913b7
Jessica Bee <bee.jess@gmail.com>
2019-01-18 00:49:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,8 @@ function ternaryCheckCity(city){
   return (city ==="NYC" ? "Ok, sounds good." : "No go.");
 }
 function switchOnCharmFromTip(tip){
-  switch 
+  switch (tip){
+    case "generous":
+      return ""
+  }
 }
