edfc84e185c28c8e0e5fee3563652527cd1583f3
Jessica Bee <bee.jess@gmail.com>
2019-01-18 00:52:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,13 @@ function switchOnCharmFromTip(tip){
   switch (tip){
     case "generous":{
       return "Thank you so much."
+      break
   }
     case "not as generous":{
       return "Thank you."
+      break
     }
-    default 
+    default: {
+      return "Bye."
+    }
 } 
